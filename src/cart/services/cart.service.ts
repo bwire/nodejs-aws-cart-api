eb207ebb441798184a5f9d20a749b7d9bf077eed
barbedwire <e.barbedwire@gmail.com>
2023-07-10 09:20:18 +0200

    integrate with cart functionality

--- src/cart/services/cart.service.ts
+++ src/cart/services/cart.service.ts
@@ -1,55 +1,51 @@
 import { Injectable } from '@nestjs/common';
-
-import { v4 } from 'uuid';
-
-import { Cart } from '../models';
+import { CartRepository } from './cart-repository';
+import { Cart, CartItem } from '../models';
 
 @Injectable()
 export class CartService {
-  private userCarts: Record<string, Cart> = {};
+  private repository = new CartRepository();
 
-  findByUserId(userId: string): Cart {
-    return this.userCarts[ userId ];
+  async findByUserId(userId: string): Promise<Cart> {
+    return this.repository.get(userId);
   }
 
-  createByUserId(userId: string) {
-    const id = v4();
-    const userCart = {
-      id,
-      items: [],
-    };
-
-    this.userCarts[ userId ] = userCart;
-
-    return userCart;
+  async createByUserId(userId: string): Promise<Cart> {
+    return this.repository.create(userId);
   }
 
-  findOrCreateByUserId(userId: string): Cart {
-    const userCart = this.findByUserId(userId);
-
+  async findOrCreateByUserId(userId: string): Promise<Cart> {
+    const userCart = await this.findByUserId(userId);
     if (userCart) {
       return userCart;
     }
 
-    return this.createByUserId(userId);
+    return await this.createByUserId(userId);
   }
 
-  updateByUserId(userId: string, { items }: Cart): Cart {
-    const { id, ...rest } = this.findOrCreateByUserId(userId);
+  async updateByUserId(userId: string, { items }: Cart): Promise<Cart> {
+    const cart: Cart = await this.findOrCreateByUserId(userId);
+
+    const newItems = items.reduce((acc, item) => {
+      const cartItem = acc.find(({product}) => product.id === item.product.id);
+
+      if (cartItem) {
+        cartItem.count = item.count;
+      } else {
+        acc.push(item);
+      }
+      return acc;
+    }, cart.items);
 
     const updatedCart = {
-      id,
-      ...rest,
-      items: [ ...items ],
+      ...cart,
+      items: newItems,
     }
 
-    this.userCarts[ userId ] = { ...updatedCart };
-
-    return { ...updatedCart };
+    return await this.repository.update(updatedCart);
   }
 
-  removeByUserId(userId): void {
-    this.userCarts[ userId ] = null;
+  async removeByUserId(userId: string): Promise<void> {
+    return this.repository.remove(userId);
   }
-
 }
